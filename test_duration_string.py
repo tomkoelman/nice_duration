208c6fca024dabc349efacadf1c698bd4739fa35
Tom Koelman <tkoelman@xs4all.nl>
2025-05-19 20:03:59 +0200

    Simplified tests by not putting them in a class

--- test_duration_string.py
+++ test_duration_string.py
@@ -1,41 +1,37 @@
 from datetime import timedelta as td
 from nice_duration import duration_string as ds
-import unittest
+import pytest
 
 
-class TestDurationStringMethods(unittest.TestCase):
-    def test_empty_timedelta(self):
-        delta = td(seconds=0)
-        self.assertEqual(ds(delta), "0s")
-        self.assertEqual(ds(delta, keep_zeroes=True), "0w0d0h0m0s")
+def test_empty_timedelta():
+    delta = td(seconds=0)
+    assert ds(delta) == "0s"
+    assert ds(delta, keep_zeroes=True) == "0w0d0h0m0s"
 
-    def test_separator(self):
-        delta = td(hours=300, minutes=70)
-        self.assertEqual(ds(delta, separator=" "), "1w 5d 13h 10m")
 
-    def test_keeping_zeroes(self):
-        delta = td(days=3, minutes=12)
-        self.assertEqual(ds(delta), "3d12m")
-        self.assertEqual(ds(delta, keep_infix_zeroes=True), "3d0h12m")
-        self.assertEqual(ds(delta, keep_leading_zeroes=True), "0w3d12m")
-        self.assertEqual(ds(delta, keep_trailing_zeroes=True), "3d12m0s")
-        self.assertEqual(
-            ds(delta, keep_leading_zeroes=True, keep_trailing_zeroes=True), "0w3d12m0s"
+def test_separator():
+    delta = td(hours=300, minutes=70)
+    assert ds(delta, separator=" ") == "1w 5d 13h 10m"
+
+
+def test_keeping_zeroes():
+    delta = td(days=3, minutes=12)
+    assert ds(delta) == "3d12m"
+    assert ds(delta, keep_infix_zeroes=True) == "3d0h12m"
+    assert ds(delta, keep_leading_zeroes=True) == "0w3d12m"
+    assert ds(delta, keep_trailing_zeroes=True) == "3d12m0s"
+    assert ds(delta, keep_leading_zeroes=True, keep_trailing_zeroes=True) == "0w3d12m0s"
+    assert (
+        ds(
+            delta,
+            keep_leading_zeroes=True,
+            keep_trailing_zeroes=True,
+            keep_infix_zeroes=True,
         )
-        self.assertEqual(
-            ds(
-                delta,
-                keep_leading_zeroes=True,
-                keep_trailing_zeroes=True,
-                keep_infix_zeroes=True,
-            ),
-            "0w3d0h12m0s",
-        )
-        self.assertEqual(ds(delta, keep_zeroes=True), "0w3d0h12m0s")
-
-    def test_call_with_number_of_seconds(self):
-        self.assertEqual(ds(70), "1m10s")
+        == "0w3d0h12m0s"
+    )
+    assert ds(delta, keep_zeroes=True) == "0w3d0h12m0s"
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_call_with_number_of_seconds():
+    assert ds(70) == "1m10s"
