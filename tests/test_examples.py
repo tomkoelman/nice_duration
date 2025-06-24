adad6c01a9e50852a0e67068d46af5ce9e6f2bb5
Tom Koelman <tkoelman@xs4all.nl>
2025-06-24 21:59:42 +0200

    Add docstring to test_examples_from_docstring

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -3,6 +3,7 @@ from nice_duration import duration_string as ds
 
 
 def test_examples_from_docstring():
+    """This tests the examples from the nice_duration docstring"""
     assert (ds(timedelta=td(hours=3, minutes=20))) == "3h20m"
     assert (ds(timedelta=td(hours=3, minutes=20), separator=" ")) == "3h 20m"
     assert (
